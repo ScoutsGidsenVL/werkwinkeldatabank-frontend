ac4530d5aa4e1213c50a2edb29ee270e44abc2df
Jeroen Wouters <jeroenwouters@MacBook-Pro.local>
2020-09-03 11:49:38 +0200

    add is sensetive to overview

--- src/components/list/workshopItem.vue
+++ src/components/list/workshopItem.vue
@@ -1,8 +1,12 @@
 <template>
-  <b-col cols="12" md='6' lg='4'>
+  <b-col cols="12" md='6' lg='4' class="workshop-item mb-3">
     <b-row class="py-4 m-2 border">
         <b-col cols="12" class="text-center">
-            {{ workshop.title }}
+            <strong>{{ workshop.title }}</strong>
+        </b-col>
+        <b-col cols="12" class="text-center">
+          <time-badge>{{ workshop.duration }}</time-badge>
+          <b-badge v-show='workshop.isSensitive' pill variant="info" class="mt-2 ml-3">Gevoelige inhoud</b-badge>
         </b-col>
         <b-col
           cols="12"
@@ -20,11 +24,21 @@
 <script lang="ts">
 import { defineComponent, PropType } from '@vue/composition-api'
 import WorkshopEntityModel from '../../models/entities/workshopEntityModel'
+import timeBadge from '../semantic/timeBadge.vue'
 
 export default defineComponent({
   name: 'workshop-item',
   props: {
     workshop: Object as PropType<WorkshopEntityModel>
+  },
+  components: {
+    timeBadge
   }
 })
 </script>
+
+<style lang='scss' scoped>
+  .workshop-item>div{
+    height: 100%;
+  }
+</style>
