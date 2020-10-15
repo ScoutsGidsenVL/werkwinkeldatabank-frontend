d8706f8647bff1f6aa833c495a0d2ae323b56eb3
Jeroen Wouters <jeroenwouters@MacBook-Pro.local>
2020-10-15 14:18:16 +0200

    remove omschrijvng building block view

--- src/views/building-blocks/view.vue
+++ src/views/building-blocks/view.vue
@@ -26,7 +26,6 @@
             <span class="w-100 h6 text-left d-inline-block" v-if="result.createdBy && can('workshops.change_buildingblocktemplate')">Gemaakt door: {{ result.createdBy.firstName }} {{ result.createdBy.lastName }}</span>
             <span class="w-100 h6 text-left d-inline-block" v-if="result.approvingTeam && can('workshops.change_buildingblocktemplate')">Gepubliceerd door {{ result.approvingTeam.title }}</span>
           </b-col>
-          <b-col cols="12" class="text-left mt-4"><strong>Omschrijving:</strong></b-col>
           <b-col cols="12" class="text-left mt-4" >
             <ckeditor-view :content='result.description' />
           </b-col>
