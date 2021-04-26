8591d48aaf2e1dc025b1853420572c8dbe58f42d
ricardo <ricardo@inuits.eu>
2021-04-26 10:15:33 +0200

    Ref #73977, Add the word 'ploeg' before displaying the team.

--- src/views/building-blocks/view.vue
+++ src/views/building-blocks/view.vue
@@ -25,7 +25,7 @@
           <b-row>
             <b-col cols="12">
               <created-by :createdBy='result.createdBy' />
-              <span class="w-100 h6 text-left d-inline-block" v-if="result.approvingTeam && can('workshops.view_field_created_by_workshop')">Gepubliceerd door {{ result.approvingTeam.title }}</span>
+              <span class="w-100 h6 text-left d-inline-block" v-if="result.approvingTeam && can('workshops.view_field_created_by_workshop')">Gepubliceerd door ploeg {{ result.approvingTeam.title }}</span>
             </b-col>
             <b-col cols="12" class="text-left mt-4" >
               <ckeditor-view :content='result.description' />
