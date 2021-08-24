e211781eda285cc46c4e08a76b63e6a688cd939c
ricardo <ricardo@inuits.eu>
2021-08-24 13:55:16 +0200

    Ref #79115, Retrieving the script url and using it to fetch the file

--- src/repositories/staticFileRepository.ts
+++ src/repositories/staticFileRepository.ts
@@ -2,11 +2,25 @@ import axios, { AxiosResponse } from 'axios'
 import store from '../store/store'
 
 export default class StaticFileRepository {
+  private getScriptUrl (): string {
+    let url = ''
+    const myScript = document.currentScript
+    if (myScript) {
+      const mySrc = myScript.getAttribute('src')
+      if (mySrc) {
+        const urlArray = mySrc.split('/')
+        url = urlArray.slice(0, urlArray.length - 1).join('/')
+      }
+    }
+
+    return url
+  }
 
   public getFile (fileUrl: string): Promise<any> {
-    return axios.get(process.env.BASE_URL + fileUrl).then(function (result: AxiosResponse) {
-      // Only return the data of response
-      return result.data
-    })
+    return axios
+      .get(this.getScriptUrl() + '/' + fileUrl)
+      .then(function (result: AxiosResponse) {
+        return result.data
+      })
   }
 }
