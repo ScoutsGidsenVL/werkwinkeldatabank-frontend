de61e88532baf65db53f0b8314fee31c33d40b6a
Jeroen Wouters <jeroenwouters@inuits.eu>
2021-09-08 13:05:14 +0200

    update token url config

--- src/main.ts
+++ src/main.ts
@@ -73,19 +73,21 @@ new StaticFileRepository().getFile('config.json').then((configFile: any) => {
         serverBaseUrl: configFile.oidc.serverBaseUrl,
         tokenEndpoint: configFile.oidc.tokenEndpoint
           ? configFile.oidc.tokenEndpoint
-          : 'token',
+          : "token",
         authEndpoint: configFile.oidc.authEndpoint
           ? configFile.oidc.authEndpoint
-          : 'auth',
+          : "auth",
         logoutEndpoint: configFile.oidc.logoutEndpoint
           ? configFile.oidc.logoutEndpoint
-          : 'logout',
+          : "logout",
         clientId: configFile.oidc.clientId,
-        authorizedRedirectRoute: '/',
-        serverTokenEndpoint: 'token/',
-        serverRefreshEndpoint: 'refresh/',
-        InternalRedirectUrl: configFile.oidc.InternalRedirectUrl ? configFile.oidc.InternalRedirectUrl : ''
-      }
+        authorizedRedirectRoute: "/",
+        serverTokenEndpoint: configFile.oidc.baseUrl + "/token/",
+        serverRefreshEndpoint: configFile.oidc.baseUrl + "/refresh/",
+        InternalRedirectUrl: configFile.oidc.InternalRedirectUrl
+          ? configFile.oidc.InternalRedirectUrl
+          : "",
+      },
     })
   }
 
